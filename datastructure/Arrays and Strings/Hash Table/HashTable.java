05731bac55510d2a16d5362b3728855b64bcb3ae
HRYUN <yunhyerim41@gmail.com>
2023-06-20 05:03:59 +0900

    Hash Table

--- datastructure/Arrays and Strings/Hash Table/HashTable.java
+++ datastructure/Arrays and Strings/Hash Table/HashTable.java
@@ -1,7 +1,7 @@
 import java.util.LinkedList;
 
 class HashTable {
-    class Node {
+    static class Node {
         String key;
         String value;
 
@@ -56,6 +56,7 @@ class HashTable {
     void put(String key, String value) {
         int hashcode = getHashCode(key);
         int index = convertToIndex(hashcode);
+        System.out.println(key +", hashcode(" + hashcode + "), index("+ index + ")");
         LinkedList<Node> list = data[index];
         if (list == null) {
             list = new LinkedList<Node>();
@@ -71,12 +72,27 @@ class HashTable {
 
     //key로 데이터를 가져오는 함수
     String get(String key) {
-        int hashcode =
+        int hashcode = getHashCode(key);
+        int index = convertToIndex(hashcode);
+        LinkedList<Node> list = data[index];
+        Node node = searchKey(list, key);
+        return node == null ? "Not Found" : node.value();
     }
 }
 
 class Test {
     public static void main(String[] args) {
+        HashTable h = new HashTable(3);
+        h.put("sung", "She is pretty");
+        h.put("jin", "She is a model");
+        h.put("hee", "She is an angel");
+        h.put("min", "She is cute");
+        h.put("sung", "She is beautiful");
 
+        System.out.println(h.get("sung"));
+        System.out.println(h.get("jin"));
+        System.out.println(h.get("hee"));
+        System.out.println(h.get("min"));
+        System.out.println(h.get("jae"));
     }
 }
